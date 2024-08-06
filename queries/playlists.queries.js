770a7c8a26f171c4c1165f08954e57fce4e5f66b
antonio <antonio.fr.martinezc@hotmail.com>
2024-08-05 21:01:54 -0400

    implemented query to get playlist with header and detail array of songs

--- queries/playlists.queries.js
+++ queries/playlists.queries.js
@@ -59,10 +59,54 @@ async function deletePlaylist(id) {
 async function getSongsInPlaylist(playlist_id) {
 	try {
 		const songs = await db.any(
-			"SELECT * FROM songs JOIN playlist_songs ON songs.id = playlist_songs.song_id WHERE playlist_songs.playlist_id = $1",
+			`SELECT
+         p.id AS playlist_id,
+         p.name AS playlist_name,
+         p.description AS playlist_description,
+         p.cover_img AS playlist_cover_img,
+         s.id AS song_id,
+         s.artist_id AS artist_id,
+         s.artist AS artist,
+         s.album_id AS album_id,
+         s.name AS song_name,
+         s.album AS album,
+         s.duration AS duration
+            
+       FROM
+         playlists p
+       JOIN
+         playlist_songs ps
+       ON
+         p.id = ps.playlist_id
+       JOIN
+         songs s
+       ON
+         s.id = ps.song_id
+       WHERE
+         p.id = $1;`,
 			playlist_id
 		);
-		return songs;
+		if (songs.length === 0) {
+			throw new Error("Playlist not found");
+		}
+		const playlist = {
+			id: songs[0].playlist_id,
+			name: songs[0].playlist_name,
+			description: songs[0].playlist_description,
+			cover_img: songs[0].playlist_cover_img,
+			tracks: songs.length,
+			songs: songs.map((item) => ({
+				id: item.song_id,
+				artist_id: item.artist_id,
+				album_id: item.album_id,
+				album: item.album,
+				name: item.song_name,
+				duration: item.duration,
+				artist: item.artist,
+			})),
+		};
+
+		return playlist;
 	} catch (error) {
 		throw error;
 	}
